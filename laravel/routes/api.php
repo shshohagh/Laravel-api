7ca5b5c8d7720463c0e3c37adc26e355de90679e
sksitfirm <sksitfirm@gmail.com>
2021-08-28 12:09:21 +0600

    My First Laravel API

---
+++ laravel/routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+//  GET
+Route::get('/posts', function(){
+    return Post::all();
+
+});
+// POST
+Route::post('/posts', function(){
+
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required',
+    ]);
+
+
+    return Post::create([
+        'title' => request('title'),
+        'content' => request('content'),
+    ]);
+});
